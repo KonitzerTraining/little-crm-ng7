4568443ab90bc5790259c9f8c0a363826e87b8be
Marcus Konitzer <mk@konitzer-training.de>
2018-11-14 13:57:31 +0100

    routing

--- little-crm/src/app/app-routing.module.ts
+++ little-crm/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartComponent} from './start/start.component';
+import {CustomerListComponent} from './customer-list/customer-list.component';
+import {ImprintComponent} from './imprint/imprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StartComponent
+  },
+  {
+    path: 'customer',
+    component: CustomerListComponent
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- little-crm/src/app/app.module.ts
+++ little-crm/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { StartComponent } from './start/start.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
+// Decorator
 @NgModule({
+  // View Classes
   declarations: [
-    AppComponent
+    // Directives, Pipes, Components
+    AppComponent,
+    StartComponent,
+    CustomerListComponent,
+    ImprintComponent
   ],
   imports: [
+    // Module
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
